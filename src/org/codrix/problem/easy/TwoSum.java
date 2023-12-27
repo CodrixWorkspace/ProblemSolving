5d426ed95a1c5bf01b272ee6b79d78e1b893b934
Prasath Basuvaraj <prasath.buddie@gmail.com>
2023-12-27 09:52:02 -0500

    Two Sum

---
+++ src/org/codrix/problem/easy/TwoSum.java
@@ -0,0 +1,30 @@
+package org.codrix.problem.easy;
+
+import java.util.Arrays;
+
+public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length ; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                if(nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        TwoSum twoSum = new TwoSum();
+        System.out.println(Arrays.toString(twoSum.twoSum(new int[]{2,7,11,15}, 9)));
+        System.out.println(Arrays.toString(twoSum.twoSum(new int[]{3,2,4}, 6)));
+        System.out.println(Arrays.toString(twoSum.twoSum(new int[]{3,3}, 6)));
+    }
+}
+
+
+
+// [2, 7, 11, 15]   9
