cb5ebb66678505e9986da97476da9d647ed99ccd
Prasath Basuvaraj <prasath.buddie@gmail.com>
2023-12-28 10:03:34 -0500

    Two Pointer Solution

--- src/org/codrix/problem/easy/RemoveDuplicate.java
+++ src/org/codrix/problem/easy/RemoveDuplicate.java
@@ -4,19 +4,19 @@ import java.util.Arrays;
 
 // 26. Remove Duplicates from Sorted Array - Lokesh
 // https://leetcode.com/problems/remove-duplicates-from-sorted-array/
-public class TwoPointer {
+public class RemoveDuplicate {
 
     public static void main(String[] args) {
-        TwoPointer twoPointer = new TwoPointer();
+        RemoveDuplicate removeDuplicate = new RemoveDuplicate();
 
         // Example usage
         int[] nums = {1, 1, 2, 2, 3, 4, 4, 5};
-        int length = twoPointer.twoPointer(nums);
+        int length = removeDuplicate.removeDuplicates(nums);
         System.out.println("Count of unique elements: " + length);
         System.out.println("Unique elements: " + Arrays.toString(Arrays.copyOf(nums, length)));
     }
 
-    public int twoPointer(int[] nums) {
+    public int removeDuplicates(int[] nums) {
         if (nums.length == 0) {
             return 0;
         }
@@ -29,5 +29,18 @@ public class TwoPointer {
             }
         }
         return j;
+    }
+
+    public int removeDuplicatesTwoPointer(int[] nums) {
+        int insertPointer = 1;
+        if (nums.length == 1)
+            return insertPointer;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] != nums[i]) {
+                nums[insertPointer] = nums[i];
+                insertPointer++;
+            }
+        }
+        return insertPointer;
     }
 }
