3f72b034439f0274f20089343479d0328095ad97
Prasath Basuvaraj <prasath.buddie@gmail.com>
2023-12-28 09:17:48 -0500

    Intercepting

--- src/org/codrix/problem/easy/RomanToInteger.java
+++ src/org/codrix/problem/easy/RomanToInteger.java
@@ -44,9 +44,10 @@ package org.codrix.problem.easy;
 
 import java.util.HashMap;
 import java.util.Map;
+
 public class RomanToInteger {
     public static int romanToInt(String s) {
-        Map<Character,Integer> m= new HashMap<>();
+        Map<Character, Integer> m = new HashMap<>();
 
         m.put('I', 1);
         m.put('V', 5);
@@ -56,21 +57,34 @@ public class RomanToInteger {
         m.put('D', 500);
         m.put('M', 1000);
 
-        int result=m.get(s.charAt(s.length()-1));
-        for(int i= s.length()-2;i>=0;i--){
-            if(m.get(s.charAt(i))<m.get((s.charAt(i+1))))
-            {
-                result-=m.get(s.charAt(i));
-            }else{
-                result+=m.get(s.charAt(i));
+
+    /*
+    Input = XXVII
+    result = 1
+
+                    i   charAt(i)   val(charAt(i))    if||else    result
+    Iteration 1     3       I              1           false       2
+    Iteration 2     2       V              5           false       7
+    Iteration 3     1       X              10          false       17
+    Iteration 4     0       X              10          false       27
+
+    * */
+
+        int result = m.get(s.charAt(s.length() - 1));
+        for (int i = s.length() - 2; i >= 0; i--) {
+            if (m.get(s.charAt(i)) < m.get((s.charAt(i + 1)))) {
+                result -= m.get(s.charAt(i));
+            } else {
+                result += m.get(s.charAt(i));
             }
         }
         return result;
     }
+
     public static void main(String[] args) {
         String romanNumeral = "XXVII";
         int result = romanToInt(romanNumeral);
-        System.out.println( result);
+        System.out.println(result);
     }
 
 }
